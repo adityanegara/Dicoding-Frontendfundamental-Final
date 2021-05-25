9445bae3a049a4c5a5ab5c2ebc25c161b8e10b92
adityanegara <aditnegarabisnis@gmail.com>
2021-05-25 14:05:29 +0700

    Custom Component : Navbar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
+import './animation/scroll_animation.js';
+import './component/Navbar/NavbarLink.js';
 
-const header = document.querySelector('.main-header');
-const menuBtn = document.querySelector('#menu-btn');
-const menuIconLine = document.querySelector('.menu-icon');
-window.addEventListener('scroll', () =>{
-    const scrollPos = window.scrollY;
-    if(scrollPos > 10){
-        header.classList.add('scrolled');
-    }else{
-        header.classList.remove('scrolled');
-    }
-})
 
